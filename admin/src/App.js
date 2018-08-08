d0a389f1cae13e70f633ee670061073b7e839a80
cmy <cmy0816@126.com>
2018-08-09 07:46:25 +0800

    '8.8'

---
+++ admin/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+  DatePicker
+} from 'antd';
+import {BrowserRouter,Router,Switch,Redirect} from 'react-router-dom';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
