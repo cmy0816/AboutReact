18f16e5c212200e4e86bc9c7b7b305728598eb0c
cmy <cmy0816@126.com>
2018-08-09 16:42:10 +0800

    '从头做人，从心开始'

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {Provider} from 'react-redux';
-import store from './store/index.js';
-import Router from './router';
+
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className = "App" >
-          <Router/>
-          </div>
-      </Provider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
--- admin/src/store/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import {
-    combineReducers
-} from 'redux';
-import user from './module/user';
-
-export default combineReducers({
-    user
-})
